ad0c4b9cff83940562cc9d6f525c9bfcbaa1322e
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-29 16:47:46 +0700

    mengimplementasi operasi CRUD

--- app/src/main/java/com/example/project8/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/project8/repository/MahasiswaRepository.kt
@@ -12,3 +12,34 @@ interface MahasiswaRepository {
     suspend fun getMahasiswaById(nim: String): Mahasiswa
 }
 
+//sebagai repository yang memanfaatkan service API untuk melakukan operasi
+class NetworkMahasiswaRepository (
+    private val MahasiswaApiService: MahasiswaService //untuk berkomunikasi dengan server melalui HTTP
+) :MahasiswaRepository{
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        MahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        MahasiswaApiService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try{
+            val response = MahasiswaApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful){
+                throw IOException("Failed to delete mahasiswa. HTTP Status code: ${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> = MahasiswaApiService.getMahasiswa()
+    override suspend fun getMahasiswaById(nim: String): Mahasiswa {
+        return MahasiswaApiService.getMahasiswaById(nim)
+    }
+}
