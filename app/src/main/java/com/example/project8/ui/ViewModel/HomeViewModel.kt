ae9bc1285065b1441cdd08243dabf86b9c531628
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-29 21:44:38 +0700

    merepresentasikan UI (sukses, error, loading)

---
+++ app/src/main/java/com/example/project8/ui/ViewModel/HomeViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.project8.ui.ViewModel
+
+import android.net.http.HttpException
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.project8.model.Mahasiswa
+import com.example.project8.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed class HomeUiState {
+    data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
+    object Error : HomeUiState()
+    object Loading : HomeUiState()
+}
