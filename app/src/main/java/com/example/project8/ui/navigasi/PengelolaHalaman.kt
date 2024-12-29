b2c60e774704e39e12ac7a25f9f5c0ff8be2a4e1
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-29 17:04:54 +0700

    mengelola navigasi home

---
+++ app/src/main/java/com/example/project8/ui/navigasi/PengelolaHalaman.kt
@@ -0,0 +1,36 @@
+package com.example.project8.ui.navigasi
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.project8.ui.view.DestinasiEntry
+import com.example.project8.ui.view.DestinasiHome
+import com.example.project8.ui.view.EntryMhsScreen
+import com.example.project8.ui.view.HomeScreen
+
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@Composable
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = {
+                }
+            )
+        }
+
+
+    }
+}
