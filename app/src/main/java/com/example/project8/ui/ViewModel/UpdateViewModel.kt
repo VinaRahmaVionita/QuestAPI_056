1816a8689e33869b55611ca2d17ebf706ffd526a
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-30 10:44:15 +0700

    Menyimpan data mahasiswa yang akan diperbarui

---
+++ app/src/main/java/com/example/project8/ui/ViewModel/UpdateViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.project8.ui.ViewModel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.project8.model.Mahasiswa
+import com.example.project8.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+
+
+data class UpdateUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
