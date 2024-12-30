15d023116294e1906249fec518ee1cb3345ff4d0
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-30 10:42:36 +0700

    menampilkan status mahasiswa

--- app/src/main/java/com/example/project8/ui/ViewModel/DetailViewModel.kt
+++ app/src/main/java/com/example/project8/ui/ViewModel/DetailViewModel.kt
@@ -13,6 +13,19 @@ import com.example.project8.ui.view.DestinasiDetail
 
 
 
+data class DetailUiState(
+    val detailUiEvent: InsertUiEvent = InsertUiEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == InsertUiEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != InsertUiEvent()
+}
+
 fun Mahasiswa.toDetailUiEvent(): InsertUiEvent{
     return InsertUiEvent(
         nim = nim,
