cf49619b9fee5a3dbde253c17b078bf069e5e5eb
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-30 10:48:11 +0700

    menjalankan MahasiswaApp

---
+++ app/src/main/java/com/example/project8/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.project8
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresExtension
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.project8.ui.ViewModel.MahasiswaApp
+import com.example.project8.ui.theme.Project8Theme
+
+class MainActivity : ComponentActivity() {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MahasiswaApp()
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Project8Theme {
+        Greeting("Android")
+    }
+}
