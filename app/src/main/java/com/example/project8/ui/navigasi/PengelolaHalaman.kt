818a445ea8ca273899b10f59afbd5b1b9196cbdb
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-30 10:30:41 +0700

    menambahkan composable detail

--- app/src/main/java/com/example/project8/ui/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/project8/ui/navigasi/PengelolaHalaman.kt
@@ -5,14 +5,19 @@ import androidx.annotation.RequiresExtension
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.project8.ui.view.DestinasiDetail
 import com.example.project8.ui.view.DestinasiEntry
 import com.example.project8.ui.view.DestinasiHome
+import com.example.project8.ui.view.DestinasiUpdate
+import com.example.project8.ui.view.DetailView
 import com.example.project8.ui.view.EntryMhsScreen
 import com.example.project8.ui.view.HomeScreen
-
+import com.example.project8.ui.view.UpdateView
 
 @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
 @Composable
@@ -26,7 +31,8 @@ fun PengelolaHalaman(
         composable(DestinasiHome.route) {
             HomeScreen(
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
-                onDetailClick = {
+                onDetailClick = {nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
                 }
             )
         }
@@ -38,6 +44,33 @@ fun PengelolaHalaman(
                     }
                 }
             })
+        }
+        composable(
+            DestinasiDetail.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiDetail.NIM){
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val nim = it.arguments?.getString(DestinasiDetail.NIM)
+            nim?.let {
+                DetailView(
+                    NavigateBack = {
+                        navController.navigate(DestinasiHome.route) {
+                            popUpTo(DestinasiHome.route) {
+                                inclusive = true
+                            }
+                        }
+                    },
+                    onEditClick =  {
+                        navController.navigate("${DestinasiUpdate.route}/$it")
+                    },
+                    onDeleteClick = {
+                        navController.popBackStack()
+                    }
+                )
+            }
         }
 
     }
