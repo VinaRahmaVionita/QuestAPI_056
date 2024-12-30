134a91885c0e608727d9fab89f32a755441c0024
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-30 10:31:27 +0700

    menginisialisasikan detail view model

--- app/src/main/java/com/example/project8/ui/ViewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/project8/ui/ViewModel/PenyediaViewModel.kt
@@ -3,6 +3,7 @@ package com.example.project8.ui.ViewModel
 import android.os.Build
 import androidx.annotation.RequiresExtension
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -13,8 +14,23 @@ object PenyediaViewModel {
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     val Factory =
         viewModelFactory {
-            initializer { HomeViewModel(aplikasiKontak().container.mahasiswaRepository) }
-            initializer { InsertViewModel(aplikasiKontak().container.mahasiswaRepository) }
+            initializer {
+                HomeViewModel(
+                    mahasiswaApplication().container.mahasiswaRepository
+                )
+            }
+            initializer {
+                InsertViewModel(
+                    mahasiswaApplication().container.mahasiswaRepository
+                )
+            }
+            initializer {
+                DetailViewModel(
+                    createSavedStateHandle(),
+                    mahasiswaApplication().container.mahasiswaRepository
+                )
+            }
+
         }
 }
-fun CreationExtras.aplikasiKontak(): MahasiswaApplications = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
+fun CreationExtras.mahasiswaApplication(): MahasiswaApplications = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
