53396c1c3c3c0c246cc79ad0e0660cb4698704b5
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-12-30 10:29:34 +0700

    membenarkan typo di baca1mahasiswa.php

--- app/src/main/java/com/example/project8/service_api/MahasiswaService.kt
+++ app/src/main/java/com/example/project8/service_api/MahasiswaService.kt
@@ -11,25 +11,22 @@ import retrofit2.http.PUT
 import retrofit2.http.Query
 
 interface MahasiswaService {
-
-    //Menyediakan header HTTP statis yang dikirimkan bersama setiap permintaan
     @Headers(
         "Accept: application/json",
-        "Content-Type: application/json",
+        "Content-Type: application/json"
     )
-
     @GET("bacamahasiswa.php")
     suspend fun getMahasiswa(): List<Mahasiswa>
 
     @GET("baca1mahasiswa.php/{nim}")
-    suspend fun getMahasiswaById(@Query("nim") nim:String):Mahasiswa
+    suspend fun getMahasiswaById(@Query("nim") nim:String): Mahasiswa
 
     @POST("insertmahasiswa.php")
-    suspend fun insertMahasiswa(@Body mahasiswa:Mahasiswa)
+    suspend fun insertMahasiswa(@Body mahasiswa: Mahasiswa)
 
     @PUT("editmahasiswa.php/{nim}")
-    suspend fun updateMahasiswa(@Query("nim")nim:String, @Body mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa(@Query("nim") nim:String, @Body mahasiswa: Mahasiswa)
 
-    @DELETE("delelemahasiswa.php/{nim}")
-    suspend fun deleteMahasiswa(@Query("nim")nim:String):Response<Void>
+    @DELETE("deletemahasiswa.php/{nim}")
+    suspend fun deleteMahasiswa(@Query("nim") nim: String): Response<Void>
 }
